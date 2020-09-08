27980dbff4e8d43a6c4b552233534dfd8ca3c44b
leewonje418 <kitti0418@naver.com>
2020-09-08 14:58:07 +0900

    commit

--- Server_p/src/api/washerCtrl/index.js
+++ Server_p/src/api/washerCtrl/index.js
@@ -1,6 +1,7 @@
 const api = require('express').Router();
 const WasherCtrl = require('./washer.ctrl');
 
-// api.post('/washerRegister', WasherCtrl.washerRegister);
+api.post('/washerRegister', WasherCtrl.washerRegister);
+api.get('/getWashers', WasherCtrl.getWashers);
 
 module.exports = api;
--- Server_p/src/api/washerCtrl/washer.ctrl.js
+++ Server_p/src/api/washerCtrl/washer.ctrl.js
@@ -1,5 +1,49 @@
 const models = require('../../models');
 exports.washerRegister = async (req, res) => {
+    console.log(req.body);
     const { floor, washerNum, way, grade, classNum, studentNum, studentName, checkWasher, washStartTime, washEndTime } = req.body;
-    
+    try {
+        const washerData = await models.Washer.findAll({
+            where: {
+                floor,
+                washerNum,
+                way,
+            },
+            raw: true
+        });
+        if(washerData.length > 0) {
+            await models.Washer.update({grade: grade, classNum: classNum, studentNum: studentNum, studentName: studentName, checkWasher: checkWasher, washStartTime: washStartTime, washEndTime: washEndTime}, {where: {floor: floor, washerNum: washerNum, way: way}})
+            const result = {
+                status: 200,
+                message: '세탁기 등록이 완료되었습니다.',
+            }
+            res.status(200).json(result);
+        } else {
+            const result = {
+                status: 403,
+                message: '잘못된 값을 전달했습니다.',
+            }
+            res.status(403).json(result);
+        }
+    } catch (error) {
+        console.error(error);
+        const result = {
+            status: 500,
+            message: '회원가입 실패'
+        }
+        res.status(500).json(result);
+    }
+};
+exports.getWashers = async (req, res) => {
+    try {
+        const Washerlist = await models.Washer.findAll();
+        res.status(200).json(Washerlist);
+    } catch (error) {
+        console.error(error);
+        const result = {
+            status: 500,
+            message: '조회 실패'
+        }
+        res.status(500).json(result);
+    }
 }
